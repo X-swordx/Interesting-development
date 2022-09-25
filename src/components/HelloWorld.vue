aaeafbe73b67d9b1e916d196ddaa58f4df2929c5
zhengchengxuan <534370078@qq.com>
2022-09-25 17:18:50 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+		<div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+			<div class="mx-auto max-w-md">
+				<h1 class="text-center text-red-500">{{msg}}</h1>
+				<div class="divide-y divide-gray-300/50">
+					<div class="space-y-6 py-8 text-base leading-7 text-gray-600">
+						<p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+						<ul class="space-y-4">
+							<li class="flex items-center">
+								<svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round"
+									stroke-linejoin="round">
+									<circle cx="12" cy="12" r="11" />
+									<path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+								</svg>
+								<p class="ml-4">
+									Customizing your
+									<code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+								</p>
+							</li>
+							<li class="flex items-center">
+								<svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round"
+									stroke-linejoin="round">
+									<circle cx="12" cy="12" r="11" />
+									<path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+								</svg>
+								<p class="ml-4">
+									Extracting classes with
+									<code class="text-sm font-bold text-gray-900">@apply</code>
+								</p>
+							</li>
+							<li class="flex items-center">
+								<svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round"
+									stroke-linejoin="round">
+									<circle cx="12" cy="12" r="11" />
+									<path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+								</svg>
+								<p class="ml-4">Code completion with instant preview</p>
+							</li>
+						</ul>
+						<p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.
+						</p>
+					</div>
+					<div class="pt-8 text-base font-semibold leading-7">
+						<p class="text-gray-900">Want to dig deeper into Tailwind?</p>
+						<p>
+							<a href="https://tailwindcss.com/docs" class="text-sky-500 hover:text-sky-600">Read the docs &rarr;</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+	export default {
+		name: 'HelloWorld',
+		props: {
+			msg: String
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
