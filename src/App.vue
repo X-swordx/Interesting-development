254e8a66f2a2576840af97bf073091c95fd59eaf
zhengchengxuan <534370078@qq.com>
2022-10-26 23:19:05 +0800

    feat：序列帧播放组件

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,42 @@
   <div id="app">
     <!-- <HelloWorld msg="Vue with tailwind"/> -->
     <!-- <PxToVw></PxToVw> -->
-    <LazyLoad></LazyLoad>
+    <!-- <LazyLoad></LazyLoad> -->
+    <MovieClip
+        class="people mx-auto"
+        ref="MovieClip"
+        :frames="imgArr"
+        :duration="2000"
+        :width="455"
+        :height="344"
+        :autoPlay="true"
+        :loop="true"
+    />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import PxToVw from './components/PxToVw.vue'
-import LazyLoad from './components/LazyLoad.vue'
+// import LazyLoad from './components/LazyLoad.vue'
+import MovieClip from './components/MovieClip/MovieClip.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
     // PxToVw,
-    LazyLoad
+    // LazyLoad,
+    MovieClip
+  },
+  data() {
+    const imgArr = [];
+    for (let index = 0; index < 16; index += 1) {
+      imgArr.push(require(`@/assets/gif/${index}.png`));
+    }
+    return {
+      imgArr
+    }
   }
 }
 </script>
