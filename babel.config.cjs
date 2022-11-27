d6e64cf1c068009dc04ffc2508d14bd49554d326
zhengchengxuan <534370078@qq.com>
2022-11-27 22:34:03 +0800

    chore：练习babel插件开发

--- babel.config.cjs
+++ babel.config.cjs
@@ -4,11 +4,21 @@ module.exports = {
   ],
   plugins: [
     [
-      require('babel-plugin-await-add-trycatch'),
-      {
-        exclude: ['build', 'node_modules'],
-        include: []
+      './lib/index.js',
+        {
+          catchFn: function() {
+            console.log('catchFn------------->')
+          },
+          exclude: ['build', 'node_modules'],
+          include: []
       }
-    ]
+    ],
+    // [
+    //   require('babel-plugin-await-add-trycatch'),
+    //   {
+    //     exclude: ['build', 'node_modules'],
+    //     include: []
+    //   }
+    // ]
   ]
 }
