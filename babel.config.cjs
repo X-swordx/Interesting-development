bbe9c7d678d26faaf9b951a475133ba387dd6f8e
zhengchengxuan <534370078@qq.com>
2022-11-30 05:18:40 +0800

    feat：add babel transform-remove-console

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,24 +1,31 @@
+
+const plugins = []
+if (process.env.NODE_ENV === 'production') {
+  // 利用 exclude 保留错误和警告信息
+  plugins.push(['transform-remove-console', {
+    'exclude': ['error', 'warn']
+  }])
+}
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  plugins: [
-    [
-      './lib/index.js',
-        {
-          catchFn: function(e) {
-            console.log('catchFn------------->', e)
-          },
-          exclude: ['build', 'node_modules'],
-          include: []
-      }
-    ],
-    // [
-    //   require('babel-plugin-await-add-trycatch'),
-    //   {
-    //     exclude: ['build', 'node_modules'],
-    //     include: []
-    //   }
-    // ]
-  ]
+  'env': {
+    'development': {
+      'plugins': ['dynamic-import-node', [
+        './lib/index.js',
+          {
+            catchFn: function(e) {
+              console.log('自定义catch------------->', e)
+            },
+            exclude: ['build', 'node_modules'],
+            include: []
+        }
+      ]
+    ]
+    },
+    'production': {
+      'plugins': ['dynamic-import-node', ...plugins]
+    }
+  }
 }
